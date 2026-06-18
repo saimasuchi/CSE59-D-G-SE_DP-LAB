eb4eee8780231645e5625bf650f49394569be60e
saimasuchi <saymasuchi3@gmail.com>
2026-06-18 16:28:56 +0600

    Update Main2.java

--- Task 2/Main2.java
+++ Task 2/Main2.java
@@ -1,1 +1,16 @@
 
+
+package Main1;
+
+ public class Main1 {
+    void check(int num){
+        System.out.println(num%1==0 && num>10);
+    }
+    
+  
+        public static void main(String[] args) {
+            Main1 checker=new Main1();
+            checker.check(14);
+        }
+        
+    }
