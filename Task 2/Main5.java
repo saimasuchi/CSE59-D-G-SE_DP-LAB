7fec9fb7ad983877144d2decf7bcb41f40449b24
saimasuchi <saymasuchi3@gmail.com>
2026-06-18 16:53:28 +0600

    Update Main5.java

--- Task 2/Main5.java
+++ Task 2/Main5.java
@@ -1,1 +1,14 @@
 
+public class Printer {
+    void printData(String text){
+        System.out.println(text);
+    }
+    void printData(int number){
+        System.out.println(number);
+    }
+    public static void main(String[] args) {
+        Printer printer=new Printer();
+        printer.printData("Hello java");
+        printer.printData(100);
+    }
+}
