c147e40530b661539d83bdcd44559903396af6f6
saimasuchi <saymasuchi3@gmail.com>
2026-06-18 16:40:30 +0600

    Update Main3.java

--- Task 2/Main3.java
+++ Task 2/Main3.java
@@ -1,1 +1,16 @@
 
+public class CountDown {
+    void start(){
+       for(int i=5;i>=1;i--){
+           System.out.println(i);
+       }
+        System.out.println("Go!");
+        
+}
+ 
+            public static void main(String[] args) {
+            CountDown countdown=new CountDown();
+            countdown.start();
+        }
+    }
+
