5bb46349fedf20e926d6f55f43a1748196fe8ad9
saimasuchi <saymasuchi3@gmail.com>
2026-06-18 16:03:48 +0600

    Fix class name typo in main method

--- Task 2/Main1.java
+++ Task 2/Main1.java
@@ -1,1 +1,19 @@
 
+
+package conteststats;
+
+public class ContestStats {
+
+  int[] scores={85, 90, 95};
+  
+  void printFirstScore(){
+      System.out.println(scores[0]);
+  }
+
+ 
+    public static void main(String[] args) {
+        ContestStates contest= new ContestStates();
+        contest.printFirstScore();
+    }
+    
+}
