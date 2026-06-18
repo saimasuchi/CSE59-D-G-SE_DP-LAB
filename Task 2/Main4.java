e0d09f8e8968f5f3a3432d128f161a89cf200637
saimasuchi <saymasuchi3@gmail.com>
2026-06-18 16:47:44 +0600

    Update Main4.java

--- Task 2/Main4.java
+++ Task 2/Main4.java
@@ -1,1 +1,15 @@
 
+public class Sensor {
+    String name;
+    int betterylevel=0;
+    void charge(){
+        betterylevel=100;
+        System.out.println("sensor fully charged");
+    }
+    
+    public static void main(String[] args) {
+        Sensor sensor=new Sensor();
+        sensor.charge();
+        System.out.println(sensor.betterylevel);
+    }
+}
